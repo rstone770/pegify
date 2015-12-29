75c19b42536153062bcb44b1bc61f5a8e3a41f53
Brenden Snyder <rstone770@gmail.com>
2015-12-28 18:17:00 -0700

    Config loader now uses a static instance of rcloader.

--- source/config.js
+++ source/config.js
@@ -1,13 +1,15 @@
 var RcLoader = require('rcloader');
 
+var loader = new RcLoader('.pegjsrc');
+
 /**
  * Returns a runtime config closest to path.
  *
  * @param  {!String} path
  * @return {!Object}
  */
-module.exports = function (path) {
-  var loader = new RcLoader('.pegjsrc');
-
+var config = function (path) {
   return loader.for(path);
 };
+
+module.exports = config;
